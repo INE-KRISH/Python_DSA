27adf70048e37edf1b2d9f8c373268f4ac173d9a
INE Codes <92663561+INE-KRISH@users.noreply.github.com>
2023-12-01 12:13:22 +0530

    Add files via upload

---
+++ List indexing and multi dimensional list.py
@@ -0,0 +1,13 @@
+List = ["Abhay", "Maan", "Mane"]
+print("\nList contains multiple values:")
+print(List)
+
+List2 = [["Mane", "Maan"],["Abhay"]]
+print("\n Multi Dimensional values:")
+print(List2)
+
+print("Accessing element from the list")
+print(List[0])
+print(List[2])
+
+#next thing is to access using negative index
