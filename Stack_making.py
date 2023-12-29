2d9b4d13a8855c854d6b35c34ebf05c308bdbaf6
INE Codes <92663561+INE-KRISH@users.noreply.github.com>
2023-12-29 09:36:14 +0530

    Add files via upload

---
+++ Stack_making.py
@@ -0,0 +1,19 @@
+stack = []
+
+stack.append('g')
+stack.append('f')
+stack.append('g')
+
+print('Inital stack')
+print(stack)
+
+print('\nElements popped from stack:')
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+
+print('\nStack after elements are popped: ')
+print(stack)
+
+
+
