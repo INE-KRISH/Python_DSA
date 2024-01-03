8e31e709054f20b2cd23d4fa99212994d9027df5
INE Codes <abhayaps05@gmail.com>
2024-01-03 12:29:57 +0530

    attic box

--- Grokkings Algorithm/Chapter 3/Attic_box_ways.py
+++ Grokkings Algorithm/Chapter 3/Attic_box_ways.py
@@ -1,6 +1,6 @@
 from numpy import empty
 
-
+#way 1
 def look_for_key(main_box):
     pile = main_box.make_a_pile_to_look_through()
     while pile is not empty:
@@ -11,5 +11,10 @@ def look_for_key(main_box):
             elif item.is_a_key():
                 print("found the key!")
                 
-                
-        
+#way 2(use of recursion)
+def look_for_key(box):
+    for item in box:
+        if item.is_a_box():
+            look_for_key(item)                      #Recursion!
+        elif item.is_a_key():
+            print("found the key!")
